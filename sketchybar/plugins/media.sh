6d1b9e2060d5e70ca91309efdb90acc17671c5b2
w1lll-dev <60063528+w1lll-dev@users.noreply.github.com>
2024-07-17 15:15:30 +0200

    Create media.sh

---
+++ sketchybar/plugins/media.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This is taken from josean-dev, he made a great video on it, check out his repo here: 
+# https://github.com/josean-dev/dev-environment-files/tree/main/.config/sketchybar
+
+STATE="$(echo "$INFO" | jq -r '.state')"
+if [ "$STATE" = "playing" ]; then
+  MEDIA="$(echo "$INFO" | jq -r '.title + " - " + .artist')"
+  sketchybar --set $NAME label="$MEDIA" drawing=on
+else
+  sketchybar --set $NAME drawing=off
+fi
