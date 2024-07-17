d1bb285e26e9fea6f83a22e286a9b31276c46cde
w1lll-dev <60063528+w1lll-dev@users.noreply.github.com>
2024-07-17 15:08:33 +0200

    Create battery.sh

---
+++ sketchybar/plugins/battery.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# This is taken from josean-dev, he made a great video on it, check out his repo here: 
+# https://github.com/josean-dev/dev-environment-files/tree/main/.config/sketchybar
+
+PERCENTAGE=$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)
+CHARGING=$(pmset -g batt | grep 'AC Power')
+
+if [ $PERCENTAGE = "" ]; then
+  exit 0
+fi
+
+case ${PERCENTAGE} in
+  9[0-9]|100) ICON="􀛨"
+  ;;
+  [6-8][0-9]) ICON="􀺸"
+  ;;
+  [3-5][0-9]) ICON="􀺶"
+  ;;
+  [1-2][0-9]) ICON="􀛩"
+  ;;
+  *) ICON="􀛪"
+esac
+
+if [[ $CHARGING != "" ]]; then
+  ICON="􀢋"
+fi
+
+# The item invoking this script (name $NAME) will get its icon and label
+# updated with the current battery status
+sketchybar --set $NAME icon="$ICON" label="${PERCENTAGE}%"
