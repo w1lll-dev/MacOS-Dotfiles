736da026b09038fd15fa14c62f876b79d9a5dc98
w1lll-dev <60063528+w1lll-dev@users.noreply.github.com>
2024-07-17 15:10:33 +0200

    Create cpu.sh

---
+++ sketchybar/plugins/cpu.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This is taken from josean-dev, he made a great video on it, check out his repo here: 
+# https://github.com/josean-dev/dev-environment-files/tree/main/.config/sketchybar
+
+CORE_COUNT=$(sysctl -n machdep.cpu.thread_count)
+CPU_INFO=$(ps -eo pcpu,user)
+CPU_SYS=$(echo "$CPU_INFO" | grep -v $(whoami) | sed "s/[^ 0-9\.]//g" | awk "{sum+=\$1} END {print sum/(100.0 * $CORE_COUNT)}")
+CPU_USER=$(echo "$CPU_INFO" | grep $(whoami) | sed "s/[^ 0-9\.]//g" | awk "{sum+=\$1} END {print sum/(100.0 * $CORE_COUNT)}")
+
+CPU_PERCENT="$(echo "$CPU_SYS $CPU_USER" | awk '{printf "%.0f\n", ($1 + $2)*100}')"
+
+sketchybar --set $NAME label="$CPU_PERCENT%"
