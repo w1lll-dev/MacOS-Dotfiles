9e94fe28ba919814d5846ea89d61d051deb6caf8
w1lll-dev <60063528+w1lll-dev@users.noreply.github.com>
2024-07-17 15:09:44 +0200

    Create calendar.sh

---
+++ sketchybar/plugins/calendar.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# This is taken from josean-dev, he made a great video on it, check out his repo here: 
+# https://github.com/josean-dev/dev-environment-files/tree/main/.config/sketchybar
+
+sketchybar --set $NAME label="$(date +'%a %d %b %H:%M')"
